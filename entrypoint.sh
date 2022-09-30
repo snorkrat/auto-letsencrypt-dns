a832d0f07aa2d9e6e4e892acf18f353fbaef08a4
snorkrat <snorkrat@gmail.com>
2022-09-30 15:22:20 +0100

    Update entrypoint.sh
    updated for dns-plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,14 @@
 
 trap "exit" SIGHUP SIGINT SIGTERM
 
+ echo "
+========================
+ __    _  _     _  _ ___
+(_ |\|/ \|_)|/ |_)|_| | 
+__)| |\_/| \|\ | \| | | 
+
+========================"
+
 if [ -z "$DOMAINS" ] ; then
   echo "No domains set, please fill -e 'DOMAINS=example.com www.example.com'"
   exit 1
@@ -12,17 +20,29 @@ if [ -z "$EMAIL" ] ; then
   exit 1
 fi
 
+if [ -z "$DNS_PLUGIN" ] ; then
+  echo "No DNS plugin set, please fill -e 'DNS_PLUGIN=eg:digitalocean'"
+  exit 1
+fi
+
+if [[ $STAGING -eq 1 ]]; then
+  echo "USING THE STAGING ENVIRONMENT"
+  ADDITIONAL="--staging"
+fi
+
 DOMAINS=(${DOMAINS})
 CERTBOT_DOMAINS=("${DOMAINS[*]/#/--domain }")
 CHECK_FREQ="${CHECK_FREQ:-30}"
-WEBROOT_PATH="${WEBROOT_PATH:-"/var/www"}"
+DNS_PLUGIN="${DNS_PLUGIN}"
+DNS_INI_PATH="${DNS_INI_PATH:-"/var/dns"}"
 
 check() {
-  echo "* Starting webroot initial certificate request script..."
+  echo "* Starting DNS Plugin initial certificate request script..."
 
-  certbot certonly --webroot --agree-tos --noninteractive --text --expand \
+  certbot certonly --agree-tos --noninteractive --text ${ADDITIONAL} --expand \
       --email ${EMAIL} \
-      --webroot-path ${WEBROOT_PATH} \
+      --dns-${DNS_PLUGIN} \
+      --dns-${DNS_PLUGIN}-credentials ${DNS_INI_PATH}/credentials.ini \
       ${CERTBOT_DOMAINS}
 
   echo "* Certificate request process finished for domain $DOMAINS"
@@ -33,18 +53,23 @@ check() {
   fi
 
   if [ "$SERVER_CONTAINER" ]; then
-    echo "* Reloading Nginx configuration on $SERVER_CONTAINER"
+    echo "* Restarting $SERVER_CONTAINER"
     eval docker kill -s HUP $SERVER_CONTAINER
   fi
 
   if [ "$SERVER_CONTAINER_LABEL" ]; then
-    echo "* Reloading Nginx configuration for label $SERVER_CONTAINER_LABEL"
+    echo "* Restarting container with label $SERVER_CONTAINER_LABEL"
 
     container_id=`docker ps --filter label=$SERVER_CONTAINER_LABEL -q`
     eval docker kill -s HUP $container_id
   fi
 
   echo "* Next check in $CHECK_FREQ days"
+
+  if [[ $STAGING -eq 1 ]]; then
+  echo "** REMINDER: This cert was generated using the STAGING environment.  For a real cert please change -e 'STAGING=0'"
+  fi
+
   sleep ${CHECK_FREQ}d
   check
 }
